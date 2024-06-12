25b2d0bb5af4d71034daef58154d698300bd14da
MarcusPirita <158137140+MarcusPirita@users.noreply.github.com>
2024-06-12 16:00:56 +0300

    Add files via upload

---
+++ Migrations/Migrations/Data/JobContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Migrations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrations.Data
+{
+    internal class JobContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=master;Trusted_Connection=True;");
+        }
+        public DbSet<Job> Jobs {  get; set; }
+    }
+}
